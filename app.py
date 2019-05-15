2e56533aeff9421f10fe01420da34e38d6b1e010
Xh Yuan <798745160@qq.com>
2019-05-15 18:45:07 +0800

    Add new line

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/home')
 def hello():
 	return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
 @app.route('/user/<name>')
 def user_page(name):
 	return '<h1>Hello %s!</h1>' % name
